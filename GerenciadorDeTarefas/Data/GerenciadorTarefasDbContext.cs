a43388d5fe0f211a7b5c489d8b0df2837b163b37
Douglas Reinaldo <Douglas39510@gmail.com>
2025-08-19 11:30:37 -0300

    Criação do projeto e implementação do banco MySql

---
+++ GerenciadorDeTarefas/Data/GerenciadorTarefasDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorDeTarefas.Data
+{
+    public class GerenciadorTarefasDbContext : DbContext
+    {
+        public GerenciadorTarefasDbContext(DbContextOptions<GerenciadorTarefasDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
